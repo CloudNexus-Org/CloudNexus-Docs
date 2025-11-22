7e55733ad5fd7e169e067c0395f16ad7f333e185
subhakanta-das <subhakantadas571@gmail.com>
2025-11-22 15:27:55 +0530

    Added inital config for docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/docs/Ariba DevOps/intro">
+            to="/docs/CN Org Structure/organizational-structure">
             View Documentation →
           </Link>
           <Link
